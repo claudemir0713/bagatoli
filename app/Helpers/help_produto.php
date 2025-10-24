b673432243b6f1deae566baaa655af800d1999ec
Claudemir Conzatti - Planner Soluçoes Empresariais <claudemir@plannersolucoes.com.br>
2025-10-24 09:03:50 -0300

    localizaCliente

--- app/Helpers/help_produto.php
+++ app/Helpers/help_produto.php
@@ -14,40 +14,56 @@ class help_produto{
         $cfop = "'101','102'";
         $sql = "
             SELECT
-                FIL.FI_RAZSOC
+                FI_RAZSOC
                 , EN_NOM
-                , NF.NFEN_DAT_ENTRAD_SAIDA
-                , NFI.INFE_PR_COD
-                , NFI.INFE_PR_NOM
-                , NFI.INFE_QTD
-                , NFI.INFE_VAL_UNIT
-                , NFI.INFE_PER_ICMS
-                , NFI.INFE_PER_PIS
-                , NFI.INFE_PER_COFINS
-                , UM_SIG_VENDA AS UND
-            FROM NF_ENTID NF
-            LEFT JOIN ITEM_NF_ENTID NFI ON NFI.EM_COD					= NF.EM_COD
-                                        AND NFI.FI_COD					= NF.FI_COD
-                                        AND NFI.EN_COD					= NF.EN_COD
-                                        AND NFI.CTDF_MODELO_FORMUL		= NF.CTDF_MODELO_FORMUL
-                                        AND NFI.CTDF_SERIE_FORMUL		= NF.CTDF_SERIE_FORMUL
-                                        AND NFI.NFEN_NUM_NF				= NF.NFEN_NUM_NF
-                                        AND NFI.NFEN_FLG_ENTRAD_SAIDA	= NF.NFEN_FLG_ENTRAD_SAIDA
+                , NFEN_DAT_ENTRAD_SAIDA
+                , INFE_PR_COD
+                , INFE_PR_NOM
+                , INFE_QTD
+                , INFE_VAL_UNIT
+                , (INFE_PER_ICMS * BASE) INFE_PER_ICMS
+                , BASE
+                , INFE_PER_PIS
+                , INFE_PER_COFINS
+                , UND
+            FROM(
+                SELECT
+                    FIL.FI_RAZSOC
+                    , EN_NOM
+                    , NF.NFEN_DAT_ENTRAD_SAIDA
+                    , NFI.INFE_PR_COD
+                    , NFI.INFE_PR_NOM
+                    , NFI.INFE_QTD
+                    , NFI.INFE_VAL_UNIT
+                    , NFI.INFE_PER_ICMS
+                    , (100-coalesce(INFE_PER_REDUC_ICMS,0))/100 BASE
+                    , NFI.INFE_PER_PIS
+                    , NFI.INFE_PER_COFINS
+                    , UM_SIG_VENDA AS UND
+                FROM NF_ENTID NF
+                LEFT JOIN ITEM_NF_ENTID NFI ON NFI.EM_COD					= NF.EM_COD
+                                            AND NFI.FI_COD					= NF.FI_COD
+                                            AND NFI.EN_COD					= NF.EN_COD
+                                            AND NFI.CTDF_MODELO_FORMUL		= NF.CTDF_MODELO_FORMUL
+                                            AND NFI.CTDF_SERIE_FORMUL		= NF.CTDF_SERIE_FORMUL
+                                            AND NFI.NFEN_NUM_NF				= NF.NFEN_NUM_NF
+                                            AND NFI.NFEN_FLG_ENTRAD_SAIDA	= NF.NFEN_FLG_ENTRAD_SAIDA
 
-            LEFT JOIN PRDUTO 	ON PRDUTO.PR_COD	= NFI.PR_COD
-            LEFT JOIN ENTID 	ON ENTID.EN_COD 	= NF.EN_COD
-            LEFT JOIN FILIAL 	ON FILIAL.FI_CNPJ	= ENTID.EN_CPF_CNPJ
-            LEFT JOIN FILIAL FIL ON FIL.EM_COD	= NF.EM_COD
-                            AND FIL.FI_COD	= NF.FI_COD
+                LEFT JOIN PRDUTO 	ON PRDUTO.PR_COD	= NFI.PR_COD
+                LEFT JOIN ENTID 	ON ENTID.EN_COD 	= NF.EN_COD
+                LEFT JOIN FILIAL 	ON FILIAL.FI_CNPJ	= ENTID.EN_CPF_CNPJ
+                LEFT JOIN FILIAL FIL ON FIL.EM_COD	= NF.EM_COD
+                                AND FIL.FI_COD	= NF.FI_COD
 
-            WHERE 1=1
-            AND NFI.PR_COD = $PR_COD
-            AND NFI.NFEN_FLG_ENTRAD_SAIDA = 'E'
-            AND FILIAL.EM_COD IS NULL
-            AND SUBSTR(TRIM(NFI.NAOP_COD_INTERN),-3) IN ($cfop)
+                WHERE 1=1
+                AND NFI.PR_COD = $PR_COD
+                AND NFI.NFEN_FLG_ENTRAD_SAIDA = 'E'
+                AND FILIAL.EM_COD IS NULL
+                AND SUBSTR(TRIM(NFI.NAOP_COD_INTERN),-3) IN ($cfop)
 
-            ORDER BY NF.NFEN_DAT_ENTRAD_SAIDA DESC
-            FETCH FIRST 3 ROWS ONLY
+                ORDER BY NF.NFEN_DAT_ENTRAD_SAIDA DESC
+                FETCH FIRST 3 ROWS ONLY
+            )DADOS
         ";
         $entrada = DB::connection('oracle')->select($sql);
         return $entrada;
